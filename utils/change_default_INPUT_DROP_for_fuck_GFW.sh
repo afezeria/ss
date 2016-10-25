5f7c51ec4aa9b26300ce0b7ca3946a3f37295f9a
JustAFakeName <nothing_to_do@gmail.com>
2016-10-24 22:10:19 -0400

    fix bug

--- utils/change_default_INPUT_DROP_for_fuck_GFW.sh
+++ utils/change_default_INPUT_DROP_for_fuck_GFW.sh
@@ -17,7 +17,7 @@ if [ $N1 -ne 1 ];then
 fi
 
 N2=$($iptables_bin -L INPUT -vn|egrep ESTABLISHED|egrep ACCEPT|wc -l)
-if [ $N2 -ne 1 ];then
+if [ $N2 -ge 1 ];then
 	$iptables_bin -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 fi
 
