12d8bb4ae848b8a6da2942802cf3d37b543e0d00
JustAFakeName <tigerbb007@gmail.com>
2017-05-30 13:22:45 +0800

    added start.sh

---
+++ utils/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cwd=$(cd `dirname $0` && pwd)
+cd $cwd
+
+apt-get install python git vim -y
+vim ./Encrypt_or_Decrypt_my_data.py
+python ./Encrypt_or_Decrypt_my_data.py new/1.sh.locked
+bash new/1.sh
