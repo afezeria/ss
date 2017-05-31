5818cc4cdd8a7b123a5d8d5f750ad5386979e6dc
JustAFakeName <nothing_to_do@gmail.com>
2017-05-31 10:28:17 +0800

    fix

--- utils/start.sh
+++ utils/start.sh
@@ -5,5 +5,5 @@ cd $cwd
 
 apt-get install python git vim -y
 vim ./Encrypt_or_Decrypt_my_data.py
-python ./Encrypt_or_Decrypt_my_data.py new/1.sh.locked
+python ./Encrypt_or_Decrypt_my_data.py -d new/1.sh.locked
 bash new/1.sh
