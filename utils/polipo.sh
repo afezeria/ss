f790d7da6649e4b24b9c0aa2cd1022f0255e4416
JustAFakeName <tigerbb007@gmail.com>
2017-07-12 17:16:29 +0800

    update polipo.sh

--- utils/polipo.sh
+++ utils/polipo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo -e "sudo polipo proxyAddress=0.0.0.0 socksParentProxy=\"127.0.0.1:1090\"  socksProxyType=socks5"
-sudo polipo proxyAddress=0.0.0.0 socksParentProxy="127.0.0.1:1090"  socksProxyType=socks5
+if [ $# -ne 4 ];then
+    echo -e "Usage:\n\t$0 \"polipo_listen_ip\" \"polipo_listen_port\" \"socksProxyIP\" \"socksProxyPort\" "
+    exit 12
+else
+    ip="$1"
+    port="$2"
+    ip2="$3"
+    port2="$4"
+fi
+
+polipo proxyAddress=$ip proxyPort=$port socksParentProxy="${ip2}:${port2}"  socksProxyType=socks5
