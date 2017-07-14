3c2f21c66c2d2d3620d2d4252edafbaa5e9420ab
JustAFakeName <tigerbb007@gmail.com>
2017-07-14 10:58:26 +0800

    added start_without_bbr.sh

---
+++ utils/start_without_bbr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cwd=$(cd `dirname $0` && pwd)
+cd $cwd
+
+apt-get install python git vim -y
+vim ./Encrypt_or_Decrypt_my_data.py
+python ./Encrypt_or_Decrypt_my_data.py -d new/1.sh.locked
+bash new/1.sh server
+#echo -e "\niptables -P INPUT ACCEPT\n" >> /etc/rc.local
+#bash new/bbr.sh
